afdb4ce67b1b6805a64d4f017e72775527bb1890
kdyook <kdyook@mobilecen.co.kr>
2024-02-16 17:21:50 +0900

    init

---
+++ src/test/java/com/together/MunDeuk/MunDeukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.together.MunDeuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunDeukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
